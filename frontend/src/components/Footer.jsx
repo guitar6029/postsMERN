6ce2dd39c9b59a0f17e68498ebb8e7c6f4f6eed1
Josh <jsbusiness305@gmail.com>
2024-12-30 11:19:15 -0500

    added footer, added styles, added react-icons, updated layout styles for nan, content, and footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { FaGithub } from "react-icons/fa";
+const Footer = () => {
+    return ( 
+        <footer className="flex flex-col w-full h-[100px] gap-2 p-4 bg-[#DF9453] hover:bg-[#CD6A65] transition duration-300 ease-in-out">
+            {/* github icon */}
+            <a className="font-semibold flex flex-row gap-2 items-center" href="https://github.com/guitar6029" target="_blank" rel="noreferrer" >My Github <FaGithub /></a>
+            <span className="font-semibold">myPosts &copy; 2025</span>
+        </footer>
+     );
+}
+ 
+export default Footer;
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,17 +1,19 @@
 import NavBar from "./Navbar";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import Footer from "./Footer";
 
 const Layout = () => {
     return (
-      <>
+      <div className="flex flex-col min-h-screen">
         <NavBar />
-        <div className="p-4">
+        <div className="flex-grow p-4">
           <Outlet />
           <ToastContainer position="bottom-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
         </div>
-      </>
-      );
+        <Footer />
+      </div>
+    );
 }
  
 export default Layout;
