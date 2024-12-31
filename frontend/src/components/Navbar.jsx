1abf2e2f4e8c371aeda9530dc1608581a7980820
Josh <jsbusiness305@gmail.com>
2024-12-30 19:28:44 -0500

    updated navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -52,12 +52,12 @@ const NavBar = () => {
 
     return (
         <>
-            <div className="flex flex-row justify-between w-full items-center gap-2 p-1 bg-[#DF9453] hover:bg-[#CD6A65] transition duration-300 ease-in-out mb-2">
+            <div className="flex flex-row justify-between w-full items-center gap-2 p-1 bg-[#424068]  transition duration-300 ease-in-out mb-2">
                 <div className="flex flex-row items-center">
                     {links.map((link, index) => (
                         <Link
                             key={index}
-                            className="text-white rounded-sm text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl p-2 font-extrabold hover:bg-[#5C211E] transition duration-300 ease-in-out"
+                            className="text-white rounded-sm text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl p-2 font-extrabold hover:underline transition duration-300 ease-in-out"
                             to={link.path}
                         >
                             {link.name}
@@ -65,7 +65,7 @@ const NavBar = () => {
                     )).slice(0, 3)}
                 </div>
                 <div className="relative">
-                    <button onClick={handleShowDropdown} className="rounded-full p-2 hover:bg-[#5C211E] transition duration-300 ease-in-out">
+                    <button onClick={handleShowDropdown} className="rounded-full p-2 ">
                         <CircleUserRound className="text-white" size={20} />
                     </button>
                     {showDropdown && (
